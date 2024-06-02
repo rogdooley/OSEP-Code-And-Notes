15660eae482f9a7ff70934a0098514a2602975a4
Roger Dooley <rogdooley@gmail.com>
2024-06-02 18:09:53 -0400

    adding command line arguments

--- Windows/Snippets/Encryption/rc4.py
+++ Windows/Snippets/Encryption/rc4.py
@@ -1,21 +1,53 @@
-from arc4 import ARC4
+import argparse
 
-# pip install arc4
+class RC4:
+    def __init__(self, key):
+        self.S = list(range(256))
+        self.key = [ord(c) for c in key]
+        self.x = 0
+        self.y = 0
+        self._ksa()
 
-def encrypt(data, key):
-    cipher = ARC4(key)
-    return cipher.encrypt(data)
+    def _ksa(self):
+        j = 0
+        key_length = len(self.key)
+        for i in range(256):
+            j = (j + self.S[i] + self.key[i % key_length]) % 256
+            self.S[i], self.S[j] = self.S[j], self.S[i]
 
-def decrypt(data, key):
-    cipher = ARC4(key)
-    return cipher.decrypt(data)
+    def _prga(self):
+        while True:
+            self.x = (self.x + 1) % 256
+            self.y = (self.y + self.S[self.x]) % 256
+            self.S[self.x], self.S[self.y] = self.S[self.y], self.S[self.x]
+            K = self.S[(self.S[self.x] + self.S[self.y]) % 256]
+            yield K
 
-# Example usage
-key = b'your-key'
-data = b'your-data'
+    def crypt(self, data):
+        data = [ord(c) for c in data]
+        keystream = self._prga()
+        return ''.join(chr(c ^ next(keystream)) for c in data)
 
-encrypted = encrypt(data, key)
-print("Encrypted:", encrypted)
 
-decrypted = decrypt(encrypted, key)
-print("Decrypted:", decrypted.decode())
+def main():
+    parser = argparse.ArgumentParser(description="RC4 encryption/decryption")
+    parser.add_argument('-k', '--key', required=True, help="The key for RC4 encryption/decryption")
+    parser.add_argument('-f', '--file', required=True, help="The input file to encrypt/decrypt")
+    parser.add_argument('-o', '--output', required=True, help="The encrypted output file")
+
+    args = parser.parse_args()
+
+    with open(args.file, 'r') as file:
+        data = file.read()
+
+    rc4 = RC4(args.key)
+    encrypted_data = rc4.crypt(data)
+
+    with open(args.output + '.enc', 'w') as file:
+        file.write(encrypted_data)
+
+    print(f"File encrypted and saved as {args.output}.enc")
+
+
+if __name__ == "__main__":
+    main()
