2172e598089cd2e6744862696db2ffe94a4842f5
Roger Dooley <rogdooley@gmail.com>
2024-07-21 15:32:51 -0400

    adding some reference assemblies

--- Windows/Powershell/xoml.ps1
+++ Windows/Powershell/xoml.ps1
@@ -3,6 +3,13 @@ $workflowasm = [Reflection.Assembly]::LoadFrom($workflowexe)
 $SerializeInputToWrapper = [Microsoft.Workflow.Compiler.CompilerWrapper].GetMethod('SerializeInputToWrapper', [Reflection.BindingFlags] 'NonPublic, Static')
 Add-Type -Path 'C:\Windows\Microsoft.NET\Framework64\v4.0.30319\System.Workflow.ComponentModel.dll'
 $compilerparam = New-Object -TypeName Workflow.ComponentModel.Compiler.WorkflowCompilerParameters
+$compilerparam.LibraryPaths.Add('C:\Windows\Microsoft.NET\Framework64\v4.0.30319')
+$compilerparam.ReferencedAssemblies.Add('System.Linq.dll')
+$compilerparam.ReferencedAssemblies.Add('System.Core.dll')
+$compilerparam.ReferencedAssemblies.Add('System.Diagnostics.Process.dll')
+$compilerparam.ReferencedAssemblies.Add('System.ServiceProcess.dll')
+$compilerparam.ReferencedAssemblies.Add('System.XML.dll')
+$compilerparam.ReferencedAssemblies.Add('System.Management.dll')
 $pathvar = "test.txt"
 $compilerparam.GenerateInMemory = $True
 $output = "C:\\Tools\\run.xml"
