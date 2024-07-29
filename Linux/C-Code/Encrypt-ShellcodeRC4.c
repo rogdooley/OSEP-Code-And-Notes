4b4c237e15e40ab7d729070a947ca33665575aca
Roger Dooley <rogdooley@gmail.com>
2024-07-29 18:40:40 -0400

    changing rc4 encryption to take arguments from command line

--- Linux/C-Code/Encrypt-ShellcodeRC4.c
+++ Linux/C-Code/Encrypt-ShellcodeRC4.c
@@ -1,39 +1,130 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <openssl/rc4.h>
 
-unsigned char key[16] = "0123456789012345"; // RC4 key
+void rc4_init(unsigned char *s, unsigned char *key, int keylen) {
+    int i, j = 0, k;
+    unsigned char temp;
 
-void encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
-             unsigned char *ciphertext) {
-    RC4_KEY rc4_key;
-    RC4_set_key(&rc4_key, strlen((char *)key), key);
-    RC4(&rc4_key, plaintext_len, plaintext, ciphertext);
+    for (i = 0; i < 256; i++) {
+        s[i] = i;
+    }
+
+    for (i = 0; i < 256; i++) {
+        j = (j + s[i] + key[i % keylen]) % 256;
+        temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+    }
 }
 
-int main() {
+void rc4_crypt(unsigned char *s, unsigned char *data, int datalen) {
+    int i = 0, j = 0, k, t;
+    unsigned char temp;
 
-     if (argc != 2) {
-        fprintf(stderr, "Usage: %s <input file>\n", argv[0]);
-        return 1;
+    for (k = 0; k < datalen; k++) {
+        i = (i + 1) % 256;
+        j = (j + s[i]) % 256;
+
+        temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+
+        t = (s[i] + s[j]) % 256;
+        data[k] ^= s[t];
+    }
+}
+
+void encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key, unsigned char *ciphertext) {
+    unsigned char s[256];
+    memcpy(ciphertext, plaintext, plaintext_len);
+    rc4_init(s, key, strlen((char *)key));
+    rc4_crypt(s, ciphertext, plaintext_len);
+}
+
+unsigned char *read_shellcode(const char *filename, int *length) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        perror("fopen");
+        return NULL;
     }
 
-    int buf_len;
-    unsigned char *buf = readBufferFromFile(argv[1], &buf_len);
-    if (buf == NULL) {
-        return 1;
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    unsigned char *buffer = malloc(file_size);
+    if (!buffer) {
+        perror("malloc");
+        fclose(file);
+        return NULL;
     }
 
-    
-    int plaintext_len = sizeof(plaintext) - 1;
-    unsigned char ciphertext[plaintext_len];
+    fread(buffer, 1, file_size, file);
+    fclose(file);
+
+    // Find the start of the shellcode
+    unsigned char *start = strstr(buffer, "unsigned char buf[] = ");
+    if (!start) {
+        fprintf(stderr, "Shellcode not found in the file.\n");
+        free(buffer);
+        return NULL;
+    }
+
+    start += strlen("unsigned char buf[] = ");
+
+    // Remove unnecessary characters and convert to raw bytes
+    unsigned char *shellcode = malloc(file_size);
+    if (!shellcode) {
+        perror("malloc");
+        free(buffer);
+        return NULL;
+    }
+
+    int count = 0;
+    for (unsigned char *p = start; *p != ';'; p++) {
+        if (*p == '\\' && *(p + 1) == 'x') {
+            unsigned int byte;
+            sscanf(p + 2, "%2x", &byte);
+            shellcode[count++] = (unsigned char)byte;
+            p += 3;
+        }
+    }
+
+    free(buffer);
+    *length = count;
+    return shellcode;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <shellcode file> <key>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    const char *filename = argv[1];
+    unsigned char *key = (unsigned char *)argv[2];
+
+    int plaintext_len;
+    unsigned char *plaintext = read_shellcode(filename, &plaintext_len);
+    if (!plaintext) {
+        return EXIT_FAILURE;
+    }
+
+    unsigned char *ciphertext = malloc(plaintext_len);
+    if (!ciphertext) {
+        perror("malloc");
+        free(plaintext);
+        return EXIT_FAILURE;
+    }
 
     encrypt(plaintext, plaintext_len, key, ciphertext);
 
     FILE *f = fopen("encoded_payload.h", "w");
     if (!f) {
         perror("fopen");
+        free(plaintext);
+        free(ciphertext);
         return EXIT_FAILURE;
     }
 
@@ -46,6 +137,9 @@ int main() {
     }
     fprintf(f, "\";\n");
     fclose(f);
+
+    free(plaintext);
+    free(ciphertext);
 
     return 0;
 }
